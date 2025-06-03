68caf59618128231457b8b59b06d5e7f102e7807
Arted0-rgb <atharvsinha1108@gmail.com>
2025-06-03 14:46:33 -0500

    Variables.py

---
+++ Lesson 1/Variables.py
@@ -0,0 +1,11 @@
+x=5
+y="John"
+print(x)
+print(y)
+name="Atharv"
+print(name)
+name=input("What is your name?")
+print("The name that you have entered is",name)
+import keyword
+print("Python keywords are:")
+print(keyword.kwlist)
